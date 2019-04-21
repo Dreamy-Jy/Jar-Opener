4c841727370ee2fbdc3d966c8bb3de859e466455
Jordane Thomas <thomasjordane7@gmail.com>
2019-04-21 03:53:46 -0400

    Timed Motion

--- code/opener/opener.ino
+++ code/opener/opener.ino
@@ -1,10 +1,15 @@
 const int ENB = 10;
 const int IN4 = 8;
 const int IN3 = 7;
-
 const int BUT = 4;
 
-bool motion = true;
+const int MOTOR_SPEED = 200;
+const int MOTOR_STOP = 0;
+bool is_running = false;
+bool is_pressed = false;
+
+unsigned long cycle_start;
+const unsigned long CYCLE_TIME = 2000;
 
 void setup() {
   pinMode(ENB, OUTPUT);
@@ -12,17 +17,24 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(BUT, INPUT);
 
-  // set inital Direction
+  // Set Motor Direction
   digitalWrite(IN4, LOW);
   digitalWrite(IN3, HIGH);
-  analogWrite(ENB, 0);
+  analogWrite(ENB, MOTOR_STOP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(BUT) == HIGH) {
-    analogWrite(ENB, 255);
-  } else {
-    analogWrite(ENB, 0);
+  is_pressed = (digitalRead(BUT) == HIGH) ? true : false;
+
+  if (!is_running && is_pressed) {
+    is_running = true;
+    start = millis();
+    analogWrite(ENB, MOTOR_SPEED);
   }
+
+  if (is_running && millis() - cycle_start >= CYCLE_TIME) {
+    is_running = false;
+    analogWrite(ENB, MOTOR_STOP);
+  }
+
 }
